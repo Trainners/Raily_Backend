6cda80e6ae6f20fce10a719ee2829f13dfe9abf3
MaTuna01 <ma775100@gmail.com>
2026-09-11 11:51:04 +0900

    init commit

---
+++ src/main/java/io/trainners/raily_backend/RailyBackendApplication.java
@@ -0,0 +1,13 @@
+package io.trainners.raily_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailyBackendApplication.class, args);
+    }
+
+}
