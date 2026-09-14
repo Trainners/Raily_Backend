ccf24b64a54c44e563a480aa47c5002530ddcd96
1234 <a1234@my-imac.local>
2026-09-14 17:13:38 +0900

    feat: swagger

--- src/main/java/io/trainners/raily_backend/domain/auth/model/dto/LoginResponse.java
+++ src/main/java/io/trainners/raily_backend/domain/auth/model/dto/LoginResponse.java
@@ -1,7 +1,5 @@
 package io.trainners.raily_backend.domain.auth.model.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
