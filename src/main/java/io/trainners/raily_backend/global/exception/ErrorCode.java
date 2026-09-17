5aaeb2bf84d473b59371e74dd1bdb53fcde0a4b6
1234 <a1234@my-imac.local>
2026-09-17 18:57:17 +0900

    feat: Error Code enum 작성

---
+++ src/main/java/io/trainners/raily_backend/global/exception/ErrorCode.java
@@ -0,0 +1,27 @@
+package io.trainners.raily_backend.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public enum ErrorCode {
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
+    REFRESH_TOKEN_MISMATCH(HttpStatus.UNAUTHORIZED, "일치하지 않는 리프레시 토큰입니다.");
+
+    private final HttpStatus status;
+    private final String message;
+
+    ErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
