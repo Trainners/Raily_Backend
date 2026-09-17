a0e399fce064375f1c8f889a0d3ebe9ccbed9ad9
1234 <a1234@my-imac.local>
2026-09-17 19:10:33 +0900

    feat: GlobalExceiptionHandler

---
+++ src/main/java/io/trainners/raily_backend/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package io.trainners.raily_backend.global.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e){
+        ErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.of(errorCode));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e){
+        String message = e.getBindingResult().getFieldError().getDefaultMessage();
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse("INVALID_REQUEST", message));
+    }
+
+}
