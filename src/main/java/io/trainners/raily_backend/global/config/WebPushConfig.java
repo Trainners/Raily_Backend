4432aae603a912cc6cbfb58b8c16eeb191c61a25
queenesun <queenesun@gmail.com>
2026-09-18 21:41:45 +0900

    [#50] - BouncyCastle 의존성 추가 및 PushService 빈 등록 수정

--- src/main/java/io/trainners/raily_backend/global/config/WebPushConfig.java
+++ src/main/java/io/trainners/raily_backend/global/config/WebPushConfig.java
@@ -1,17 +1,22 @@
 package io.trainners.raily_backend.global.config;
 
 import nl.martijndwars.webpush.PushService;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.security.GeneralSecurityException;
+import java.security.Security;
 
 @Configuration
 @EnableConfigurationProperties(WebPushProperties.class)
 public class WebPushConfig {
     @Bean
     public PushService pushService(WebPushProperties props) throws GeneralSecurityException {
+        if (Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) == null) {
+            Security.addProvider(new BouncyCastleProvider());
+        }
         return new PushService(props.publicKey(), props.privateKey(), props.subject());
     }
 }
