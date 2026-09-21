55a5b2045a5fa586b6f267eae399907c65ccca18
1234 <a1234@my-imac.local>
2026-09-21 10:54:44 +0900

    feat: 예외 타입 교체

--- src/main/java/io/trainners/raily_backend/domain/auth/service/CustomUserDetailsService.java
+++ src/main/java/io/trainners/raily_backend/domain/auth/service/CustomUserDetailsService.java
@@ -3,11 +3,10 @@ package io.trainners.raily_backend.domain.auth.service;
 import io.trainners.raily_backend.domain.auth.model.CustomUserDetails;
 import io.trainners.raily_backend.domain.user.model.entity.User;
 import io.trainners.raily_backend.domain.user.repository.UserRepository;
-import io.trainners.raily_backend.global.exception.BusinessException;
-import io.trainners.raily_backend.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,7 +18,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email){
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+                .orElseThrow(() -> new UsernameNotFoundException("인증 실패"));
 
         return new CustomUserDetails(user);
     }
